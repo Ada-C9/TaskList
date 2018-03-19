5fcaab15d589ad5d3688df82d091b274fe65761e
Kate Pond <kate@thepondsedge.com>
2018-03-19 16:11:06 -0700

    creates a list of tasks and puts them on index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
+
+  TASK_LIST = [
+    { id:1, task: "Clean the apt", description: "Use the vaccum!", sub_tasks: ["vaccum", "dust", "laundry"] },
+    { id:2, task: "Go shopping", description: "Go to the store!", sub_tasks: ["vaccum", "dust", "laundry"] },
+    { id:4, task: "Go outside", description: "Go outdoors!", sub_tasks: ["hike", "yard work", "relax"] },
+    { id:5, task: "Eat Ice Cream", description: "Eat more ice cream!", sub_tasks: ["get ice cream", "eat ice cream"]}
+  ]
+
   def index
+    @tasks = TASK_LIST
   end
 
   def show
