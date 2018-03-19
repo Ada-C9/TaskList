ac8712cb15764d46cdd21810a07857a40beeaabd
Brenda Rios <riosbrenda@gmail.com>
2018-03-19 16:12:25 -0700

    completed baseline for Taskslist.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+
+  def index
+
+
+    @tasks = ["Cleaning", "Laundry", "Cooking", "Dishes"]
+    
+  end
+
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+
+  end
+
+  def destroy
+
+  end
+
+end
