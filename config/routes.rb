390f65a7c93b8774f4818382c9e291198fa4698a
Kate Pond <kate@thepondsedge.com>
2018-03-19 15:23:08 -0700

    creates pages and updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   patch 'tasks/:id', to: 'tasks#mark_complete'
 
-  get 'tasks/:id', to: 'tasks#destroy'
+  get 'tasks/:id', to: 'tasks#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
