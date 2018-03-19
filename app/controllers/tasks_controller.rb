028eb05b98771096ecfda8176a89fc82cfdfbea5
Phoebe <phoebe.manning8@gmail.com>
2018-03-19 15:13:04 -0700

    creates controller action for index page and an index.html.erb page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,13 @@
 class TasksController < ApplicationController
+  TASKS_LIST = [
+    {id: 1, title: "Task One"},
+    {id: 2, title: "Task Two"},
+    {id: 3, title: "Task Three"},
+    {id: 4, title: "Task Four"}
+  ]
+
   def index
+    @tasks = TASKS_LIST
   end
 
   def show
