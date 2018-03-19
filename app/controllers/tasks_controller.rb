a9bcbdf0baf31d1828db5b1eb397f7adcc564297
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-19 15:28:07 -0700

    added index to TaskController

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,16 @@
 class TasksController < ApplicationController
+
+  TASKS_LIST = [
+    {id: 1, title: "Do CS Fun homework", due: Date.new(2018,3,26),
+      status: :incomplete},
+    {id: 2, title: "Pay cellphone bill", due: Date.new(2018,3,29),
+      status: :incomplete},
+    {id: 3, title: "Call Dad", due: Date.new(2018,3,19), status: :incomplete},
+    {id: 4, title: "Feed fish", due: Date.new(2018,3,18), status: :complete}
+  ]
+
   def index
+    @tasks = TASKS_LIST
   end
 
   def show
