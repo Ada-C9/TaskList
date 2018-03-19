35e6298d9a9e962abe3bddbdee7ffee6e0bbf574
Kat <kat.eastman@gmail.com>
2018-03-19 15:49:39 -0700

    finished baseline

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
   def index
+    @tasks = [{task: "read textbook", status: "incomplete"},{task: "do laundry", status: "incomplete"}, {task: "finish website", status: "incomplete"}, {task: "practice derby edges", status: "incomplete"}]
   end
 end
