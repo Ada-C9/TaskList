3143056615fe24d8eec7b55aa84bbc2537491242
Kate Pond <kate@thepondsedge.com>
2018-03-19 15:20:19 -0700

    creates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  get '/tasks', to: 'tasks#index'
 
-  get 'tasks/show'
+  get 'tasks/show', to: 'tasks#show', as: 'task'
 
-  get 'tasks/create'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks/create', to: 'tasks#create'
 
-  get 'tasks/edit'
+  get 'tasks/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
 
-  get 'tasks/mark_complete'
+  patch 'tasks/:id', to: 'tasks#mark_complete'
 
-  get 'tasks/delete'
+  get 'tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
