d6ae23a36f54b8664d4e8a31c9cf652a55d8e32e
Mary Lamkin <mmlamkin@gmail.com>
2018-03-19 15:31:56 -0700

    generated controllers and methods, set up routes, created an ERB view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = ["clean room", "vacuum", "post office", "wash car"]
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def mark_complete
+  end
+end
