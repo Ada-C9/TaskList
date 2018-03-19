ce4a68060073b696323ca2b1d87ca3de433d6ee7
Anne <annehwatson@gmail.com>
2018-03-19 16:03:50 -0700

    added route for task/index.html.erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
   def index
+    @tasks = [
+      { task_name: "Vacuum", status: "Incomplete" },
+      { task_name: "Run dishwasher", status: "Incomplete" },
+      { task_name: "Do homework", status: "Incomplete" }
+    ]
   end
 end
