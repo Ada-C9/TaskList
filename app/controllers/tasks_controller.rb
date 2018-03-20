cee2750c6104fd7e23d6681f32214961d6fb7c0d
Kat <kat.eastman@gmail.com>
2018-03-20 13:46:45 -0700

    Added Wave 1 elements.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,5 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [
-      {task: "read textbook", status: :INCOMPLETE},
-      {task: "do laundry", status: :INCOMPLETE},
-      {task: "finish website", status: :INCOMPLETE},
-      {task: "practice derby edges", status: :INCOMPLETE},
-    ]
+    @tasks = Task.all
   end
 end
