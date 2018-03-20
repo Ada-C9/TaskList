dbf8dc88afe2c7b3ff6a8560a393bd898253e882
Kate Pond <kate@thepondsedge.com>
2018-03-20 14:12:46 -0700

    updates app to read from the DB

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,12 @@
 class TasksController < ApplicationController
 
-  TASK_LIST = [
-    { id:1, task: "Clean the apt", description: "Use the vaccum!", sub_tasks: ["vaccum", "dust", "laundry"] },
-    { id:2, task: "Go shopping", description: "Go to the store!", sub_tasks: ["vaccum", "dust", "laundry"] },
-    { id:4, task: "Go outside", description: "Go outdoors!", sub_tasks: ["hike", "yard work", "relax"] },
-    { id:5, task: "Eat Ice Cream", description: "Eat more ice cream!", sub_tasks: ["get ice cream", "eat ice cream"]}
-  ]
-
   def index
-    @tasks = TASK_LIST
+    @tasks = Task.all
   end
 
   def show
     id = params[:id]
-    @tasks = TASK_LIST.find do |task|
-      task[:id] == id.to_i
-    end
+    @tasks = Task.find(id)
   end
 
   def create
