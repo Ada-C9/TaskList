72a468431c0b2828f3286821dec6247e18fc18f1
Phoebe <phoebe.manning8@gmail.com>
2018-03-20 14:15:05 -0700

    adds three tasks using rails console

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,12 @@
 class TasksController < ApplicationController
-  TASKS_LIST = [
-    {id: 1, title: "Task One"},
-    {id: 2, title: "Task Two"},
-    {id: 3, title: "Task Three"},
-    {id: 4, title: "Task Four"}
-  ]
 
   def index
-    @tasks = TASKS_LIST
+    @tasks = Task.all
   end
 
   def show
     id = params[:id]
-    @task = TASKS_LIST.find do |task|
-      task[:id] == id.to_i
-    end
+    @task = Task.find(id)
   end
 
   def new
