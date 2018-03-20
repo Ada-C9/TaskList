d10e912032d4f875d1a6fd430a0184f4bb046f81
Ari <dominiquearia0@gmail.com>
2018-03-20 14:52:32 -0700

    Created instances of Task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
   def index
-    @home_list = ["feed the llama", "dust the sheets", "sweep the roof", "make bubble soup"]
+    @tasks = Task.all
   end
 end
