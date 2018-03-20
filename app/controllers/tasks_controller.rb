967f94f72f9d9a42903d4a236602b4213ae6dd2f
Alexandria Brown <axbn2190@gmail.com>
2018-03-20 16:57:10 -0700

    added delete option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,16 @@
 class TasksController < ApplicationController
 
-  TASK_LIST = [{id: 1, title: "Make pasta", description: "This is where you describe the task of making pasta.", status: "Incomplete"}, {id: 2, title: "Do laundry", description: "This is where you describe the task of doing laundry.", status: "Incomplete"}, {id: 3, title: "Read POODR", description: "This is where you describe the task of reading POODR.", status: "Incomplete"}]
-
   def index
-    @tasks = TASK_LIST
+    @tasks = Task.all
   end
 
   def show
     id = params[:id]
-    @task = TASK_LIST.find { |task| task[:id] == id.to_i }
+    @task = Task.find(id)
   end
 
   def new
+    
   end
 
   def create
@@ -24,6 +23,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    id = params[:id]
+    @task = Task.find(id)
+    @task.destroy
   end
 
 end
