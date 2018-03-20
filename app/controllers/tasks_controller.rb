2ee7f60f6f4995afc4947cec0f7bbaf559c0a6bd
LetiTran <leticiastran@gmail.com>
2018-03-20 14:07:53 -0700

    Changed views to show the tasks created + make each of them a link to it self intead of just a string.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,7 @@
 class TasksController < ApplicationController
 
-  TASK_LIST = [
-    {id: 1, name: "Take dog out", status: "Incomplete"},
-    {id: 2, name: "Cook breakfast", status: "Incomplete"},
-    {id: 3, name: "Take a shower", status: "Incomplete"},
-    {id: 4, name: "Pack lunch", status: "Incomplete"}
-  ]
-
   def index
-    @tasks = TASK_LIST
+    @tasks = Task.all
   end
 
   def show
