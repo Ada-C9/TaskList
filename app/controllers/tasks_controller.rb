24258681b031ba06323aa72a8af1c4348580c8e8
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-03-20 15:39:33 -0700

    Updated methods/action as database model is added and changed html.erb files accordingly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,12 @@
 class TasksController < ApplicationController
-  Task_LIST = [{ id:1, task_type:"cleaning", done_by:"George", complete: "true" }, { id:2, task_type:"cooking", done_by:"Jane Austin", complete: "false"}, { id:3, task_type:"organizing", done_by:"JK Rowling", complete: "false"}, { id:4, task_type:"reading", done_by:"Octavia E. Butler", complete: "true"}]
 
   def index
-    @tasks = Task_LIST
+    @tasks = Task.all
   end
 
   def show
     id = params[:id]
-    @task = Task_LIST.find do |task|
-      task[:id] == id.to_i
-    end
+    @task = Task.find(id)
   end
 
   def new
@@ -25,5 +22,8 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    id = params[:id]
+    @task = Task.find(id)
+    @task.destroy
   end
 end
