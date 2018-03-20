364b5a0ece78aae921d42ef683403de8c1ee26d7
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-03-20 16:52:56 -0700

    Create task controller, model, migrate and tests

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    id = params[:id]
+    @task = Task.find(id)
+
+  end
+
+  def new
+    
+  end
+end
