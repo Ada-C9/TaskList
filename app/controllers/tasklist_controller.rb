0ac042225b30c1beac1ab2deaeb8264ec92711fb
Kiera <kiera.thomasson@gmail.com>
2018-03-19 19:50:50 -0700

    initial set up of tasklist controller and index.html.erb

---
+++ app/controllers/tasklist_controller.rb
@@ -0,0 +1,31 @@
+class TasklistController < ApplicationController
+  def index
+    @tasklist = [
+      "clean bathroom", "study for an hour", "wash the dishes", "do laundry", "wash hair", "buy new sneakers"
+    ]
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
