6ba649aa10a6bb4b0e751d6177c4cee05d261490
AngelaPoland <angelalpoland@gmail.com>
2018-03-19 17:18:39 -0700

    created more in index, show, and some CSS

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+
+  TASKS_LIST = [
+    {id: 1, task: "do the thing", misc_notes: "stuff about this task", completed: false},
+    {id: 2, task: "do the thing", misc_notes: "stuff about this task", completed: false},
+    {id: 3, task: "walk the puppy", misc_notes: "stuff about this task", completed: false},
+    {id: 4, task: "finish that poodr book", misc_notes: "secret agenda", completed: false}
+  ]
+
+  def index
+    @tasks = TASKS_LIST
+  end
+
+  def show
+    id = params[:id]
+    @task = TASKS_LIST.find do |task|
+      task[:id] == id.to_i
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
