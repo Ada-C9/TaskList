3d3e7f4928dd5918c6ff404b5777231e08debac4
Jackie <jonofre16@hotmail.com>
2018-03-20 14:32:06 -0700

    Added methods destroy, edit and create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,22 @@
 class TasksController < ApplicationController
-  TASKS_LIST = [
-    {id: 1, task: "Clean the bathroom"},
-    {id: 2, task: "Wash Dishes"},
-    {id: 3, task: "Sweep the floor"},
-    {id: 4, task: "Vacuum"},
-    {id: 5, task: "Finish homework"}
-  ]
   def index
-    @tasks = TASKS_LIST
+    @tasks = Task.all
   end
 
   def show
     id = params[:id]
-    @task = TASKS_LIST.find do |task|
-      task[:id] == id.to_i
-    end
+    @task = Task.find(id)
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+    id = params[:id]
+    @task = Task.find(id)
+    @task.destroy
   end
 end
