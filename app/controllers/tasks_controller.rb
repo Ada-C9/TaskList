e1788c8b326227e2bb4c8a5095ffe99040b724fd
Karinna Iniguez <karinnainiguez@gmail.com>
2018-03-21 15:43:12 -0700

    Add functionality to add new task to list, and show details about each task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,44 @@ def index
   @tasks = Task.all
 end
 
+def new
+  @task = Task.new
+end
+
+def create
+  raw_task = params[:task]
+
+  task = Task.new
+  task.name = raw_task[:name]
+  task.description = raw_task[:description]
+
+  if task.save
+    redirect_to '/tasks'
+  end
+end
+
+def show
+  task_id = params[:id]
+
+  @task = Task.find(task_id)
+end
+
+def edit
+  @task = Task.find(params[:id])
+end
+
+def update
+  raw_task = params[:task]
+  task = Task.find(params[:id])
+  task.name = raw_task[:name]
+  task.description = raw_task[:description]
+
+  if task.save
+    redirect_to task_path(task)
+  end
+end
+
+def destroy
+end
+
 end
