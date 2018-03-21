1e9e378104bfee938b836c10314c9a387ed2e7f4
Kat <kat.eastman@gmail.com>
2018-03-21 14:16:40 -0700

    added resting routes, and forms for new and edit.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,49 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+
+  def create
+    raw_task = params[:task]
+    task = Task.new
+
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+    task.completion_date = raw_task[:completion_date]
+
+    if task.save
+      redirect_to '/tasks'
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def update
+    raw_task = params[:task]
+    task = Task.find(params[:id])
+
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+    task.completion_date = raw_task[:completion_date]
+
+    if task.save
+      redirect_to '/tasks'
+    end
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # always do collection before individual ones
-  get '/tasks/new', to: 'tasks#new'
-  get '/tasks', to: 'tasks#index'
-  post '/tasks', to: 'tasks#create'
+  root "tasks#index"
 
-# individual
-  get '/tasks/:id', to: 'tasks#show'
-  get '/tasks/:id/edit', to: 'tasks#edit'
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks", to: "tasks#create", as: "tasks"
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
   patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
+
+  get "/tasks", to: "tasks#index"
+  get "/tasks/:id", to: "tasks#show", as: "task"
+
+  delete "/tasks/:id", to: "tasks#destroy", as: "delete_task"
+
 end
