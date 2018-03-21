c3a7d8c88026ac92707efdec9361c5e548cd76b5
Kate Pond <kate@thepondsedge.com>
2018-03-20 23:16:47 -0700

    corrects some routes to new_task

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
 
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post 'tasks/create', to: 'tasks#create'
 
   get 'tasks/edit', to: 'tasks#edit', as: 'edit_task'
   patch '/tasks/:id', to: 'tasks#update'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   patch 'tasks/:id', to: 'tasks#mark_complete'
 
