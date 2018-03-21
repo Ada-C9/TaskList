b43bf0a5e576617d874d2b211aca94da04fd289a
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-21 14:58:05 -0700

    Nearly all Wave 2 functionality added, save for optional styling. Still need to test the task creation mechanism, however.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,28 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    id = params[:id]
+    @task = Task.find(id)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  task = Task.new
+  task.name = params[:task][:name]
+  task.completion_date = params[:task][:completion_date]
+  task.description = params[:task][:description]
+
+  if task.save #it worked
+    redirect_to books_path
+  else
+    render :new
+  end
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'tasks/index', to: 'tasks#index', as: 'index'
+  get '/tasks/index', to: 'tasks#index', as: 'index'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
