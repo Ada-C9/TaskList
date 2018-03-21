a0b966ea66e9ee54fdf2565b3bf6404ccd32bcef
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-03-21 12:21:26 -0700

    Create route name, controller action, and ERB view for show action and update index action to link to show action for each individual task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,23 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new; end
+
+  def create
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find(task_id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
