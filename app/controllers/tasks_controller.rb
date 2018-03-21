c1e755a69a02224d80535b76a7dd4423f5c49cd1
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-03-21 12:38:20 -0700

    Create route name and ERB view containing a form for new action and update Task controller create action to create and save a new task and redirect to the list of all tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,12 @@ class TasksController < ApplicationController
   def new; end
 
   def create
+    new_task = Task.new(name: params[:name], description: params[:description], completion_date: params[:completion_date])
+
+    if new_task.save
+      redirect_to tasks_path
+    end
+
   end
 
   def show
