6c5ae8f7c925002c5cb59ae90a91260ed2bd1ca6
Hannah Cameron <hannahiscoding@gmail.com>
2018-03-21 15:52:57 -0700

    added in ability to create a new task; added link to index to access new task page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,36 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+  end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    raw_task = params[:task]
+    task = Task.new
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+
+    if task.save
+      redirect_to '/tasks'
+    end
+  end
+
+  def show
+    task_id = params[:id]
+
+    @task = Task.find(task_id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
   end
 
 end
