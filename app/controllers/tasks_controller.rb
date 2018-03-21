618a8fbe368b26aae03e029d985face65f82102e
Aruna <aruna.thungathurthi@gmail.com>
2018-03-21 14:35:13 -0700

    wave2 completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,33 @@
 class TasksController < ApplicationController
+  TASKS_LIST = [
+    {id:1,title:"Cleaning",description:"incomplete"},
+    {id:2,title:"Washing",description:"complete"},
+    {id:3,title:"Cooking",description:"complete"}]
   def index
+    @tasks = Task.all
   end
 
   def show
+    id = params[:id]
+        @task = Task.find(id)
+
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+
+    task= Task.new
+    task.title=params[:task][:title]
+    task.description= params[:task][:description]
+    task.completion_date=params[:task][:completion_date]
+    if task.save
+      redirect_to tasks_path
+    else
+      render:new
+    end
   end
 
   def edit
