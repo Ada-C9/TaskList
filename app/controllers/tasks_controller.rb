4cca4a86d1770e556138e9480cd21392eb9e86f7
Kate Pond <kate@thepondsedge.com>
2018-03-21 15:06:57 -0700

    creates form for a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,20 @@ class TasksController < ApplicationController
     @tasks = Task.find(id)
   end
 
+  def new
+    @task = Task.new
+  end
+
   def create
+    task = Task.new
+    task.title = params[:task][:title]
+    task.description = params[:task][:description]
+    task.completion_date = params[:task][:completion_date]
+
+    if task.save
+      redirect_to tasks_path
+    end
+
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
 
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post 'tasks/create', to: 'tasks#create'
+  post '/tasks', to: 'tasks#create'
 
   get 'tasks/edit', to: 'tasks#edit', as: 'edit_task'
   patch '/tasks/:id', to: 'tasks#update'
