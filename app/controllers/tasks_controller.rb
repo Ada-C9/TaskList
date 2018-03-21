ffd7a870b9a39719dd660e6afc3920a8f8f80496
Tor Shimizu <torshimizu@gmail.com>
2018-03-21 15:19:26 -0700

    updated tasks_controller to have methods for edit, show, update, create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,21 +4,43 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    raw_task = params[:task]
+    task = Task.new
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+    task.priority = raw_task[:priority]
+    task.completion_date = raw_task[:completion_date]
+
+    if task.save
+      redirect_to '/task'
+    end
   end
 
   def show
-    task_id = param[:id]
+    task_id = params[:id]
 
     @task = Task.find(task_id)
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    task = Task.find(params[:id])
+    raw_task = params[:task]
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+    task.priority = raw_task[:priority]
+    task.completion_date = raw_task[:completion_date]
+
+    if task.save
+      redirect_to '/tasks'
+    end
   end
 
   def destroy
