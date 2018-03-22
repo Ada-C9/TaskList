b4677bd284f39299f5972184ba8b2fcfdfdd287b
emilcecarlisa <emilcecarlisa@gmail.com>
2018-03-22 16:37:50 -0700

    Created links to views for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,23 +3,30 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  # def new
-  # end
+  def new
+    @task = Task.new
+  end
 
-  # def create
-  # end
-
-  def show
-    # is there a hash created (once a form exits?)
-    # there's a name, description and completion_date
-    # books included title and author
+  def create
+  # this method doesn't send response to the browser, it redirects to the tasks view
+  # by default, the controller will send you to whatever controller shares the name
     raw_task = params[:task]
 
     task = Task.new # must create a local instance of a book
 
-    task.name = raw_task[:description]
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
     task.completion_date = raw_task[:completion_date]
 
+    if task.save
+      redirect_to '/tasks'
+      # redirect_to books_path # is another way of redirecting
+    end
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find(task_id)
   end
 
   # def edit
@@ -27,7 +34,7 @@ class TasksController < ApplicationController
 
   # def update
   # end
-  
+
   # def destroy
   # end
 
