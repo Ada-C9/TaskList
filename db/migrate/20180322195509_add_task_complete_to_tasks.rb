58eebe046c106b0c60d6427c1341147b3142f982
Phoebe <phoebe.manning8@gmail.com>
2018-03-22 13:04:32 -0700

    adds migration for task_complete option

---
+++ db/migrate/20180322195509_add_task_complete_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddTaskCompleteToTasks < ActiveRecord::Migration[5.1]
+  def change
+    # add_column :books, :task_complete, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320210045) do
+ActiveRecord::Schema.define(version: 20180322200229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180320210045) do
     t.string "comp_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "task_complete"
   end
 
 end
