21cb88a08cff7c3633a005bb52b3a0d4cccabaf5
LetiTran <leticiastran@gmail.com>
2018-03-22 14:02:21 -0700

    Changed root/homepgae to be the tasklist page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'tasks#index'
+  # root_path exists
+
   get '/tasks', to: 'tasks#index', as: 'tasks'
 
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
