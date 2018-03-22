b6c526618ce694dceeff8dafd591f695dcd0c573
Samantha Berk <sberk10@gmail.com>
2018-03-22 16:09:02 -0700

    created a migration to add a column to the database schema that addresses whether the task is completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320214014) do
+ActiveRecord::Schema.define(version: 20180322230425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180320214014) do
     t.date "complete_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status"
   end
 
 end
