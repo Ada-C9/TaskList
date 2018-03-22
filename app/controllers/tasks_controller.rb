16fb8f8c98b3fa7ab9b33c9c95877e6a9805fc26
Anne <annehwatson@gmail.com>
2018-03-22 13:20:14 -0700

    changed some css for overall and individual tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,40 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [
-      { task_name: "Vacuum", status: "Incomplete" },
-      { task_name: "Run dishwasher", status: "Incomplete" },
-      { task_name: "Do homework", status: "Incomplete" }
-    ]
+    @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(name: params[:name], description: params[:description])
+    if task.save
+      redirect_to "/tasks"
+    end
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find(task_id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def update
+    form_data = params[:task]
+
+    task = Task.find(params[:id])
+    task.name = form_data[:name]
+    task.description = form_data[:description]
+    task.completion_date = form_data[:completion_date]
+
+    if task.save
+      redirect_to task
+    end
+  end
+
 end
