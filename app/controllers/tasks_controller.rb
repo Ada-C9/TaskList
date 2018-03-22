6976e53b6258f54651f0590ddf76abbb826e4d49
Ari <dominiquearia0@gmail.com>
2018-03-22 10:15:16 -0700

    added form for in new and edit.html page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,52 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    raw_task = params[:task]
+
+    task = Task.new
+    task.name = raw_task[:name]
+    task.details = raw_task[:details]
+    task.completed = raw_task[:completed]
+
+    if task.save
+      redirect_to '/tasks'
+    end
+  end
+
+  def show
+    task_id = params[:id]
+    @task = Task.find(task_id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    raw_book = params[:task]
+
+    task = Task.find(params[:id])
+
+    task.assign_attributes(
+      task.name = raw_task[:name],
+      task.details = raw_task[:details],
+      task.completed = raw_task[:completed]
+    )
+
+    if task.save
+      redirect_to book_path(task)
+    end
+  end
+
+    def destroy
+    end
+
 end
