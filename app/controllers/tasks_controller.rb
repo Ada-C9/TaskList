cb3596e2a0e6abeaabcd5af438736475f8c8fe31
knockknockhusthere <angela.huxia@gmail.com>
2018-03-21 22:53:43 -0700

    creates/saves a new task and redirects to index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,23 +3,53 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def create
+  def new
+    @task = Task.new
   end
 
-  def new
+  def create
+    name = params[:task][:name]
+
+    raw_task = params[:task]
+
+    task = Task.new
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+    task.duedate = raw_task[:duedate]
+    task.complete = raw_task[:complete]
+
+    if task.save
+      redirect_to tasks_path
+    end
   end
 
   def show
     task_id = params[:id]
-    # Load it from the DB
-    # Save it in an instance variable for the view
+
     @task = Task.find(task_id)
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    raw_task = params[:task]
+
+    task = Task.find(params[:id])
+    task.name = raw_task[:name]
+    task.description = raw_task[:description]
+    task.duedate = raw_task[:duedate]
+    task.complete = raw_task[:complete]
+
+    if task.save
+      redirect_to task_path
+    end
   end
 
   def destroy
+    task = Task.find(:id)
+    task.destroy
   end
 
 end
