6bb06f156e9fa7cb71dea97f6b343e8656412306
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-03-21 21:33:02 -0700

    Did the edit action , making the method, template, form_for, and used some radio buttons.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,21 @@ class TasksController < ApplicationController
 
     @task = Task.find(my_task)
   end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    incoming_task = params[:task]
+    new_task = Task.new
+    new_task.name = incoming_task[:name]
+    new_task.description = incoming_task[:description]
+    new_task.completion = incoming_task[:completion]
+    new_task.priority = incoming_task[:priority]
+
+    if new_task.save
+      redirect_to '/tasks'
+    end 
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: "all_tasks"
+  get '/tasks/new', to: 'tasks#new', as: "new_task"
+  post '/tasks', to: 'tasks#create'
+
   get '/tasks/:id', to: 'tasks#show', as: "task_detail"
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "task_edit"
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#delete'
+
 end
