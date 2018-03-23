b5c5f45743029c89d9df4cc321fc93a75830df69
Brenda Rios <riosbrenda@gmail.com>
2018-03-23 15:09:54 -0700

    I made a migration to create a new column named completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320220428) do
+ActiveRecord::Schema.define(version: 20180323220552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180320220428) do
     t.date "completion_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
   end
 
 end
