77b81ed225cfd17248b2ed52b9b83d832e803c2d
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-03-22 17:12:12 -0700

    Add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   patch '/tasks/:id', to: 'tasks#update'
-  get 'tasks/destroy'
+  delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
