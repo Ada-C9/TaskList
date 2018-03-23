1d98ea29a0fc4697f066fdbe1481656371c4bd22
Nora Peters <norapeters5@gmail.com>
2018-03-22 19:37:05 -0700

    put route created for toggle_complete action. css class name results from conditional statement looking for a completion date, triggered by the toggle#complete action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(:id)
   end
 
   def show
@@ -10,21 +10,50 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    task = Task.new
+    task.name = params[:task][:name]
+    task.description = params[:task][:description]
+    task.priority = params[:task][:priority]
+    if task.save # this saves the new book AND evaluates the conditional
+      redirect_to tasks_path
+    else
+      render :new # this symbol is just referring to the name of the view. Not going to trigger new cycle, will just bring up that view.
+    end
   end
 
   def edit
+    @task = Task.find_by(id: params[:id])
   end
 
   def update
+    task = Task.find_by(id: params[:id])
+    if !task.nil?
+      if task.update(name: params[:task][:name], description: params[:task][:description], priority: params[:task][:priority])
+        redirect_to task_path(task.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to tasks_path
+    end
+  end
+
+  def toggle_complete
+    id = params[:id]
+    task = Task.find(id)
+    task.update(completion_date: Date.new)
+    redirect_to tasks_path
   end
 
   def destroy
     id = params[:id]
     @task = Task.find(id)
     @task.destroy
+    redirect_to tasks_path
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'books#destroy'
+  put '/tasks/:id/', to: 'tasks#toggle_complete', as: 'complete_task'
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
