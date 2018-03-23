f9bfd652b638d1971a4ab330a5e7577cf5108190
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-23 14:16:57 -0700

    added strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,20 +14,32 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new
-    @task.name = params[:task][:name]
-    @task.status = params[:task][:status]
-    @task.description = params[:task][:description]
-    @task.due = params[:task][:due]
+    @task = Task.new(task_params)
+    # @task.name = params[:task][:name]
+    # @task.status = params[:task][:status]
+    # @task.description = params[:task][:description]
+    # @task.due = params[:task][:due]
     if @task.save
-      redirect_to task_path
+      redirect_to tasks_path
     end
   end
 
   def edit
+    @task = Task.find_by(id: params[:id])
   end
 
   def update
+    @task = Task.find_by(id: params[:id])
+    redirect_to tasks_path if @task.nil?
+    # @task.name = params[:task][:name]
+    # @task.status = params[:task][:status]
+    # @task.description = params[:task][:description]
+    # @task.due = params[:task][:due]
+    if @task.update(task_params)
+      redirect_to task_path(@task.id)
+    else
+      render :edit
+    end
   end
 
   def mark_complete
@@ -37,8 +49,15 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    id = params[:id]
-    @task = Task.all.find { |task| task.id == id.to_i }
-    @task.destroy
+    Task.find(params[:id]).destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  # def
+
+  def task_params
+    return params.require(:task).permit(:name, :status, :description, :due)
   end
 end
