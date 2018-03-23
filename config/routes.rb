8096047f1dd4c75bb8d51580d61aa5fb0ea88600
Tor Shimizu <torshimizu@gmail.com>
2018-03-23 12:23:25 -0700

    added partials for new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'tasks#index'
-  # resources :tasks
-  get '/tasks', to:'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 
   patch 'tasks/:id/complete', to: 'tasks#mark_complete', as: 'task_complete'
 end
