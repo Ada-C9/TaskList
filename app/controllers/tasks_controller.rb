12405e7236d7bb055923e2d5bf1695f934bb7082
Abiaina <abinnet.a@gmail.com>
2018-03-23 15:18:10 -0700

    partial form DRYs edit and new views, confirm Delete button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,52 @@
 class TasksController < ApplicationController
 
-  TASKS = ["feed cat", "read poodr", "make dinner', renew bus pass", "laundry", "return library books", "charge phone", "finish hw"]
-
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new
+    task.name = params[:task][:name]
+    task.description = params[:task][:description]
+    task.completion_date = params[:task][:completion_date]
+
+    if task.save
+      redirect_to show_tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    task = Task.find_by(id: params[:id])
+    task.name = params[:task][:name]
+    task.completion_date = params[:task][:completion_date]
+    task.description = params[:task][:description]
+    if task.save
+      redirect_to show_tasks_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    if @task.destroy
+      redirect_to show_tasks_path
+    else
+      render :destroy
+    end
+  end
 end
---
+++ db/migrate/20180321225254_add_id.rb
@@ -0,0 +1,4 @@
+class AddId < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
