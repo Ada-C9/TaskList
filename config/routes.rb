8e340f57ee17e8fbdf7d1947b52ad4f92ef48fe4
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-03-23 13:59:44 -0700

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'tasks#index'
+  #root_path exists
+
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
   post 'tasks/', to: 'tasks#create', as: 'create_task'
