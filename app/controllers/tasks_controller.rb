ff6217bf1443485a1bc0a5d653e57f66bb364265
Mary Lamkin <mmlamkin@gmail.com>
2018-03-23 13:29:36 -0700

    partial view forms

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,17 +5,17 @@ class TasksController < ApplicationController
 
   def show
     id = params[:id]
-    @tasks = Task.find(id)
+    @task = Task.find(id)
   end
 
   def new
-    @tasks = Task.new
+    @task = Task.new
   end
 
   def create
-    task = Task.new
-    task.task_name = params[:task][:task_name]
-    task.description = params[:task][:description]
+    task = Task.new(task_params)
+    # task.task_name = params[:task][:task_name]
+    # task.description = params[:task][:description]
     if task.save
       redirect_to tasks_path
     else
@@ -24,14 +24,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
     task = Task.find(params[:id])
-    task.task_name = params[:task][:task_name]
-    task.description = params[:task][:description]
-    if task.save
+    # task.task_name = params[:task][:task_name]
+    # task.description = params[:task][:description]
+    if task.update(task_params)
       redirect_to tasks_path
     else
       render :edit
@@ -40,11 +40,24 @@ class TasksController < ApplicationController
 
   def destroy
     id = params[:id]
-    @tasks = Task.find(id)
-    @tasks.destroy
+    @task = Task.find(id)
+    @task.destroy
+    redirect_to tasks_path
   end
 
   def mark_complete
+    @task = Task.find(params[:id])
+    @task.completion_date = DateTime.now
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :show
+    end
+  end
 
+  private
+
+  def task_params
+    return params.require(:task).permit(:task_name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   get 'tasks/:id/mark_complete', to: 'tasks#mark_complete', as: "mark_complete"
-
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
