d1a18fbaea40804938694b278f7b0af2ceb0d426
Kat <kat.eastman@gmail.com>
2018-03-23 23:14:12 -0700

    trying to remove extraneous files

--- db/migrate/20180322172041_add_target_date_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddTargetDateToTasks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :tasks, :target_date, :datetime
-  end
-end
--- db/migrate/20180322172420_change_data_type_for_completion_date.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataTypeForCompletionDate < ActiveRecord::Migration[5.1]
-  def change
-    change_column :tasks, :completion_date, :datetime
-    change_column :tasks, :target_date, :date
-  end
-end
