0a1596c9131aea9ed5078eef901df2918a9de229
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-03-23 20:07:21 -0700

    did a completion method, so when marked as completed if date was nil it should assign Date.now to that attribute. And if you click on marked as completed when completed, should change the time attribute to nil. Then the index view shows one thing if completion is nil or not

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,13 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     my_task = params[:id]
 
     @task = Task.find(my_task)
   end
+
   def new
     @task = Task.new
   end
@@ -20,8 +22,53 @@ class TasksController < ApplicationController
     new_task.priority = incoming_task[:priority]
 
     if new_task.save
-      redirect_to '/tasks'
-    end 
-
+      redirect_to all_tasks_path
+    end
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    updated_task = params[:task]
+    the_task = Task.find(params[:id])
+    # the_book.title = updated_book[:title]
+    # the_book.author = updated_book[:author]
+    # the_book.publication_date = updated_book[:publication_date]
+    # the_book.synopsys = updated_book[:synopsys]
+    the_task.update_attributes(
+      name: updated_task[:name],
+      description: updated_task[:description],
+      completion: updated_task[:completion],
+      priority: updated_task[:priority],
+    )
+
+    if the_task.save
+      redirect_to tasks_path
+    end
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to tasks_path
+  end
+
+  def mark_complete
+    task = Task.find(params[:id])
+    if task.completion == nil
+      task.update_attributes(completion: Time.now)
+    else
+      task.update_attributes(completion: nil)
+
+      redirect_to tasks_path
+
+    end
+  end
+
+  private
+  def task_params
+    return params.require(:task).permit(:name, :description, :completion, :priority)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index', as: "all_tasks"
+  root 'tasks#index'
+
+  get '/tasks', to: 'tasks#index', as: "tasks"
   get '/tasks/new', to: 'tasks#new', as: "new_task"
   post '/tasks', to: 'tasks#create'
 
-  get '/tasks/:id', to: 'tasks#show', as: "task_detail"
-  get '/tasks/:id/edit', to: 'tasks#edit', as: "task_edit"
+  get '/tasks/:id', to: 'tasks#show', as: "task"
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "edit_task"
+  put '/tasks/:id', to: 'tasks#mark_complete'
   patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#delete'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 end
