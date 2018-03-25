98c649d5e3602240b93f1797e06742798faf13d7
Phoebe <phoebe.manning8@gmail.com>
2018-03-25 11:35:21 -0700

    changes update route from patch to put

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-  patch '/tasks/:id', to: 'tasks#complete'
+
   patch '/tasks/:id', to: 'tasks#update'
+  put '/tasks/:id', to: 'tasks#complete'
   delete '/tasks/:id', to: 'tasks#destroy'
 
 
