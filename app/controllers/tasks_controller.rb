a68af4deed2a662e6581e4e8d9fb0c68d11253c8
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-03-25 16:26:13 -0700

    Finished Wave 2, created Method#show with appropriate view, completed Method#new with appropriate view and form.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,20 @@ class TasksController < ApplicationController
     @task = Task.find(task_id)
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+   raw_task = params[:task]
+
+   task = Task.new
+   task.name = raw_task[:name]
+   task.time_of_day = raw_task[:time_of_day]
+   task.completed = raw_task[:completed]
+
+   if task.save
+     redirect_to '/tasks'
+   end
+ end
 end
