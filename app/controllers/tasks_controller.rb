0604c568bb95830fe014c58783f357bdcbcd8002
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-26 15:46:20 -0700

    cleaned up css on all tasks, accounting for completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,8 +29,8 @@ class TasksController < ApplicationController
 
   def mark_complete
     @task = Task.find(params[:id])
-    @task.update(status: "Complete")
-    @task.update(completed_on: Time.now)
+    @task.update(status: "Complete", completed_on: Time.now)
+    # @task.update(completed_on: Time.now)
     @task.save
     redirect_to task_path(@task.id)
   end
