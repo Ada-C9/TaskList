3f2545a2f31efac287c3e1af4e033567cefe50c2
Steffnay <steffannbrown@gmail.com>
2018-03-26 09:36:28 -0700

    added completed method and updated css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,13 +15,58 @@ class TasksController < ApplicationController
 
   def create
     task = Task.new
-    task.name = params[:task][:name]
+    task.name = params[:task][:name].capitalize
     task.description= params[:task][:description]
-    task.completed = params[:task][:complete_by]
+    task.complete_by = params[:task][:complete_by]
+    task.completed = nil
     task.save
 
     if task.save
       redirect_to tasks_path
     end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+
+    if !@task.nil?
+      if @task.update(name: params[:task][:name], description: params[:task][:description], complete_by: params[:task][:complete_by])
+
+        redirect_to task_path(@task.id)
+
+      else
+        render :edit
+      end
+    else
+      redirect_to tasks_path
+    end
+  end
+
+  def completed
+    @task = Task.find_by(id: params[:id])
+
+    if !@task.nil?
+      @task.completed = Date.today
+      @task.save
+
+      redirect_to task_path
+
+    else
+      redirect_to task_path
+
+    end
+
+  end
+
+  def destroy
+    id = params[:id]
+    @task = Task.find(id)
+    @task.destroy
+
+  redirect_to tasks_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,12 @@ get '/tasks/:id', to:'tasks#show', as: 'task'
 
 post '/tasks', to: 'tasks#create'
 
+get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+patch '/tasks/:id', to: 'tasks#update'
+
+delete '/tasks/:id', to: 'tasks#destroy'
+
+patch '/tasks/:id/completed', to: 'tasks#completed', as: 'task_completed'
 
 end
