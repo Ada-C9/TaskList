317751d179551a0f21a9c80b7c55a9e2d4e5a3cc
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-03-25 17:06:58 -0700

    Add routes for mark complete and unmark

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  patch 'tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
+
+  patch 'tasks/:id/unmark_complete', to: 'tasks#unmark_complete', as: 'unmark_complete'
+
   post 'tasks/', to: 'tasks#create', as: 'create_task'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
