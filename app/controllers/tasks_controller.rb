45e84695c2abbfdef3fb55c284229e9080cf7761
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-26 02:52:11 -0700

    Edit and delete functions are working, but the 'Mark Complete' button is still broken.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,10 +14,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new
-    task.name = params[:task][:name]
-    task.completion_date = params[:task][:completion_date]
-    task.description = params[:task][:description]
+    task = Task.new(task_params)
+    # Commented out so we can use task_params
+    # task.name = params[:task][:name]
+    # task.completion_date = params[:task][:completion_date]
+    # task.description = params[:task][:description]
 
     if task.save #it worked
       redirect_to index_path
@@ -26,9 +27,45 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      redirect_to task_path
+    end
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if !@task.nil?
+
+      # Materials commented out because we are now using strong params.
+      # @book.update(title: params[:book][:title], author: params[:book][:author], description: params[:book][:description])
+
+      if @task.update(task_params)
+        redirect_to task_path(@task.id)
+      else
+        render :edit
+      end
+    else
+      redirect_to index_path
+    end
+  end
+
+  def mark_complete
+    @task = Task.find_by(id: params[:id])
+    @task.update_attribute(:completion_date, Time.now)
+    redirect_to index_path
+    # if @task.completion_date.kind_of?(Time)
+    #   redirect_to index_path
+    # else
+    #   render :edit
+    # end
+  end
+
   def destroy
     id = params[:id]
     @task = Task.find(id)
+
     if @task.destroy #it worked
       redirect_to index_path
     # else
@@ -36,5 +73,11 @@ class TasksController < ApplicationController
     end
   end
 
+  private
 
+  def task_params
+    # You don't need an explicit return here, but if it helps keep things straight in your head, it's okay to do it.
+    return params.require(:task).permit(:name, :completion_date, :description)
+    # If you want to add new fields, you have to add them here, not in the create or update methods.
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get '/tasks/index', to: 'tasks#index', as: 'index'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
-
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-
+  patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
+  patch '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
