0f710bc9ff85af0be4957c3482f1337f90359acc
LetiTran <leticiastran@gmail.com>
2018-04-03 10:15:30 -0700

    Using resources in routes instead of hard-coded routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
 
+  patch '/tasks/:id/complete', to: 'tasks#complete', as: 'mark_complete'
+  resources :tasks
+
   root 'tasks#index'
   # root_path exists
+  #
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  #
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  #
+  # post '/tasks', to: 'tasks#create'
+  #
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  #
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  # patch '/tasks/:id', to: 'tasks#update'
+  #
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-  patch '/tasks/:id', to: 'tasks#update'
-
-  delete '/tasks/:id', to: 'tasks#destroy'
-
-  patch '/tasks/:id/complete', to: 'tasks#complete', as: 'mark_complete'
+  # patch '/tasks/:id/complete', to: 'tasks#complete', as: 'mark_complete'
 end
