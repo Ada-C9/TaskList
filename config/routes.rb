454ba20a4a7e8af2f0ebcc66eb4b32f0ec5ab2dc
Lily Sky <lilyxsky@gmail.com>
2018-04-03 10:11:37 -0700

    rails resources used instead of each route specifically written out

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,29 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index', as: 'tasks'
 
   get '/tasks/all', to: 'tasks#all', as: 'all_tasks'
 
-  get '/tasks/new', to: 'tasks#new', as: "new_task"
-
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-  patch '/tasks/:id', to: 'tasks#update'
-
   patch '/tasks/completed/:id', to: 'tasks#completed', as: 'completed_task'
 
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
+  root 'tasks#index'
+
+
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  #
+  # get '/tasks/all', to: 'tasks#all', as: 'all_tasks'
+  #
+  # get '/tasks/new', to: 'tasks#new', as: "new_task"
+  #
+  # post '/tasks', to: 'tasks#create'
+  #
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  #
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  # patch '/tasks/:id', to: 'tasks#update'
+  #
+  # patch '/tasks/completed/:id', to: 'tasks#completed', as: 'completed_task'
+  #
+  # delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
