1accc8530310c4d01db1dab6a8ee3e20dd9bc426
Alexandria Brown <axbn2190@gmail.com>
2018-04-05 10:01:55 -0700

    stylesheet rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
+  resources :tasks, except: [:destroy]
+
   root 'tasks#index'
 
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new-task'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  post '/tasks', to: 'tasks#create'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit-task'
-  patch '/tasks/:id', to: 'tasks#update'
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new-task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # post '/tasks', to: 'tasks#create'
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit-task'
+  # patch '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
