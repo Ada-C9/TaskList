725cca10cc2b78dfb368ac1831f58f592128b6f7
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-05 10:00:01 -0700

    Fixed css for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
 
+
 get '/tasks', to: 'tasks#index', as: 'tasks'
 get '/tasks/new', to: 'tasks#new', as: 'new_task'
 post '/tasks', to: 'tasks#create'
