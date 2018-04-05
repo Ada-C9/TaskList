9a854529aff1770953e9a5c604bfb08e83e6cf7c
Jackie <jonofre16@hotmail.com>
2018-04-05 10:04:30 -0700

    Edited the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  get 'tasks/', to: 'tasks#index', as: 'tasks'
+  resources :tasks
+  root to: 'tasks#index'
 
-  post '/tasks', to: 'tasks#create'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # get 'tasks/', to: 'tasks#index', as: 'tasks'
+  #
+  # post '/tasks', to: 'tasks#create'
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  #
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
   patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
-  patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
