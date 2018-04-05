984b1792a5e41e6ccc6bac0567de52572a49a15a
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-05 16:22:44 -0700

    test for images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.cache_classes = true
-  config.serve_static_assets = true
-  config.assets.compile = true
-  config.assets.digest = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -91,4 +87,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
 end
