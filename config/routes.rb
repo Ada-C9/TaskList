3236515354b7cd4511096658a9ec57ca190103ca
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-08 02:14:50 -0700

    Mention path to edit button

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,22 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
   #root_path exists
-
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get 'tasks/new', to: 'tasks#new', as: 'new_task'
   patch 'tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
 
   patch 'tasks/:id/unmark_complete', to: 'tasks#unmark_complete', as: 'unmark_complete'
+  resources :tasks
 
-  post 'tasks/', to: 'tasks#create', as: 'create_task'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/:id', to: 'tasks#destroy'
 
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # patch 'tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
+  #
+  # patch 'tasks/:id/unmark_complete', to: 'tasks#unmark_complete', as: 'unmark_complete'
+  #
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # patch '/tasks/:id', to: 'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  #
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
